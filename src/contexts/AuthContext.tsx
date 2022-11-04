d9ebe58cd983c185e1cc64a49539c5c7e55e75be
Maurício de Carvalho <mauriciocr223@gmail.com>
2022-11-04 17:30:47 -0300

    feat: created authentication with google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -17,18 +23,44 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      "938977022592-u8dskco3mfbj5codsjjoba3695j3ker0.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ["profile", "email"],
+  });
+
   async function signIn() {
-    console.log("Vamos logar!");
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log("TOKEN DE AUTENTICAÇÃO", access_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: "Maurício",
-          avatarUrl: "https://github.com/mauriciocr22.png",
-        },
+        isUserLoading,
+        user,
       }}
     >
       {children}
